b30e8bcd9f330d904d3c9a9d3beff40767d53438
nick paridon <paridonn1@gmail.com>
2018-10-13 10:06:59 -0400

    multer

--- src/server/routes/file-routes.js
+++ src/server/routes/file-routes.js
@@ -1,20 +1,17 @@
-var dealController = require('../controllers/dealController');
-var companyController = require('../controllers/companyController');
 var contactController = require('../controllers/contactController');
 var userController = require('../controllers/userController');
-var commentController = require('../controllers/commentController');
-var updateController = require('../controllers/updateController');
+
 var models = require('../models');
 var cloudinary = require('cloudinary');
 var fs = require('fs');
-
+var multer = require('multer');
+var upload = multer({ dest: 'uploads/' });
+var path = require('path');
 cloudinary.config({
 	cloud: process.env.CLOUDINARY_CLOUD_NAME,
 	api_key: process.env.CLOUDINARY_API_KEY,
 	api_secret: process.env.CLOUDINARY_API_SECRET
 });
-var multer = require('multer');
-var upload = multer({ dest: '../uploads' });
 
 module.exports = function(app) {
 	app.post('/api/contacts/:id/addPhoto', upload.single('avatar'), function(req, res) {
@@ -22,7 +19,7 @@ module.exports = function(app) {
 			if (error) {
 				console.log(error);
 			}
-			fs.unlink('../uploads/' + req.file.filename, (err) => {
+			fs.unlink('uploads/' + req.file.filename, (err) => {
 				if (err) {
 					console.log('failed to delete local image:' + err);
 				} else {
